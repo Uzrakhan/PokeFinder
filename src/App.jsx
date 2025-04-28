b486e6362b9e99f337a1d7cb672ef1f770a0cffa
Uzrakhan <uzrakhan539@gmail.com>
2025-04-28 20:43:28 +0530

    Added robust error handling & state management

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import './App.css'
 import PokemonCard from './components/PokemonCard';
 import SearchBar from './components/SearchBar';
 import TypeFilter from './components/TypeFilter';
+import { ClipLoader } from 'react-spinners'
+
 
 function App() {
   const [pokemon,setPokemon] = useState([]);
-  const [loading,setLoading] = useState(false);
+  const [loading,setLoading] = useState(true);
+  const [error,setError] = useState(null);
   const [searchTerm,setSearchTerm] = useState('');
   const [selectedType, setSelectedType] = useState('all');
 
@@ -21,23 +24,67 @@ function App() {
     return matchesSearch && matchesType
   });
 
+  const renderContent = () => {
+    if(error) {
+      return(
+        <div className="text-red-400 text-center p-8">
+          <h2 className="text-2xl font-bold mb-4">Error!</h2>
+          <p >{error}</p>
+          <p className="mt-4">Refresh the page again.</p>
+        </div>
+      )
+    }
+
+    if(loading) {
+      return(
+        <div className="text-center mt-12">
+          <ClipLoader color="#FFFFFF" size={50}/>
+          <p className="text-white mt-4">Catching Pokemon...</p>
+        </div>
+      )
+    }
+
+    if(filteredPokemon.length === 0) {
+      return(
+        <div className="text-yellow-400 text-center p-8">
+          <h3 className="text-2xl font-bold mb-4">No Pokemon Found.</h3>
+          <p>Try adjusting search or filter settings.</p>
+        </div>
+      )
+    }
+
+    return(
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {filteredPokemon.map((p) => (
+          <PokemonCard key={p.id} pokemon={p}/>
+        ))}
+      </div>
+    )
+  };
+
+
   useEffect(() => {
     const fetchData = async () => {
       try{
         const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=150');
-        const data = await response.json();
+        if(!response.ok) {
+          throw new Error(`HTTP error. status:${response.status}`)
+        };
 
+        const data = await response.json();
 
         const details = await Promise.all(
           data.results.map(async (p) => {
             const res = await fetch(p.url);
+            if(!res.ok) throw new Error(`Failed to fetch ${p.name}`)
             return res.json();
           })
         );
         setPokemon(details);
-        setLoading(false)
       }catch(error){
-        console.error('API error',error);
+        setError(error.message)
+      }finally{
+        setLoading(false)
       }
     };
     fetchData();
@@ -52,15 +99,7 @@ function App() {
       <SearchBar onSearchChange={setSearchTerm}/>
       <TypeFilter onTypeChange={setSelectedType} types={allTypes}/>
 
-      {loading ? (
-        <p className="text-white text-center">Loading...</p>
-      ): (
-        <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-          {filteredPokemon.map((p) => (
-            <PokemonCard pokemon={p}/>
-          ))}
-        </div>
-      )}
+      {renderContent()}
     </div>
   )
 }
